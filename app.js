2af6099fa4fe40b868976f9c6335eafc30b75cf0
UrielBlanco7 <urielblanco175@gmail>
2023-02-08 11:59:46 -0300

    Subiendo Archivos

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const port = process.env.PORT || 8000;
+const start = () => console.log("On http://localhost:8000");
+app.listen(port, start);
+const publico = path.resolve(__dirname, "./public");
+app.use(express.static(publico));
+
+
+app.listen(8000, () => console.log("Servidor levantado en puerto 8000"));
+
+
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "views/home.html"));
+});
+app.get("/login", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "views/login.html"));
+});
+app.get("/register", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "views/register.html"));
+});
